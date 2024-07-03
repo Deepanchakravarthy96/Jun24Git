76565770df039861831e47c09b1bbd2a6bf56546
Deepanchakravarthy96 <deepanking9696@gmail.com>
2024-07-03 15:24:28 +0530

    Sample

--- src/test/java/org/demo/StudentInfo.java
+++ src/test/java/org/demo/StudentInfo.java
@@ -32,5 +32,9 @@ public class StudentInfo {
 		System.out.println(nextByte + nextByte2 + nextByte3 + nextByte4 + nextByte5);
 
 	}
+	private void Company() {
+
+		System.out.println("Company name is tcs");
+	}
 
 }
